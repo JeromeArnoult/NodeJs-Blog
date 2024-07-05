cd989a2d468b85a5131b40b5d055c9e9249af87f
JeromeArnoult <jerome.arnoult.pro@proton.me>
2024-07-05 07:39:51 +0200

    debug

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const path = require('path');
+const mongoose = require('mongoose');
 const multer = require('multer');
+const path = require('path');
 const connectDB = require('./server/config/db');
 const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
@@ -15,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 /* Connect to DB */
+mongoose.set('strictQuery', false);
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
@@ -22,17 +24,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 
-/* Configure Multer for local storage */
-const storage = multer.memoryStorage();  // Use memory storage to avoid EROFS error
+// Configuration de multer pour le stockage en mémoire
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
-    }),
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
 }));
 
 app.use(express.static('public'));
@@ -45,13 +47,13 @@ app.set('view engine', 'ejs');
 app.locals.isActiveRoute = isActiveRoute;
 
 app.use((req, res, next) => {
-    res.locals.currentRoute = req.path;
-    next();
+  res.locals.currentRoute = req.path;
+  next();
 });
 
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
